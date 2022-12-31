d0eea811cc8672a3fe417bf78aa4b62a87843b83
koolleeo <86853558+koolleeo@users.noreply.github.com>
2022-12-31 12:56:32 +0000

    javascript updated. Added an array of timeblock objects

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,43 @@
+//create an array of timeblock objects
+
+const calenderRow = [
+    {
+        textArea: "#text-area-9am",
+        hour: 09
+    },
+    {
+        textArea: "#text-area-10am",
+        hour: 10
+    },
+    {
+        textArea: "#text-area-11am",
+        hour: 11
+    },
+    {
+        textArea: "#text-area-12pm",
+        hour: 12
+    },
+    {
+        textArea: "#text-area-13pm",
+        hour: 13
+    },
+    {
+        textArea: "#text-area-14pm",
+        hour: 14
+    },
+    {
+        textArea: "#text-area-15pm",
+        hour: 15
+    },
+    {
+        textArea: "#text-area-16pm",
+        hour: 16
+    },
+    {
+        textArea: "#text-area-17pm",
+        hour: 17
+    }
+];
 
 //function to display current datetime to page, and track current hour
 function displayClock() {
